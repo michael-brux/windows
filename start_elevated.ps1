651628cc37f543dc3b3f045b21f856df3cd74108
Michael Brux <7270100+michael-brux@users.noreply.github.com>
2025-04-04 21:30:53 +0200

    Update start_elevated.ps1

--- start_elevated.ps1
+++ start_elevated.ps1
@@ -9,8 +9,8 @@ if (-Not (Get-Command $pwshPath -ErrorAction SilentlyContinue)) {
 }
 
 if (-Not $isAdmin) {
-    # Re-launch the script with administrator privileges
-    $script = [System.Diagnostics.Process]::Start($pwshPath, "-Command Start-Process $pwshPath -Verb RunAs")
+    # Re-launch the script with administrator privileges in Windows Terminal
+    $script = [System.Diagnostics.Process]::Start("wt.exe", "powershell -Command Start-Process $pwshPath -ArgumentList '-File', '$(Get-Command $MyInvocation.MyCommand.Definition).Path' -Verb RunAs")
     exit
 } else {
     Write-Host "PowerShell is running with elevated privileges."
