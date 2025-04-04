1b707b6c4da740cc39581adcade75abde6b954da
Michael Brux <7270100+michael-brux@users.noreply.github.com>
2025-04-04 21:12:03 +0200

    Update install_openssh.ps1

--- install_openssh.ps1
+++ install_openssh.ps1
@@ -1,5 +1,13 @@
 # Script to install OpenSSH client and server on Windows
 
+# Check for administrative privileges
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    # Re-launch the script with elevated privileges
+    Start-Process powershell "-File $PSCommandPath" -Verb RunAs
+    Exit
+}
+
 # Install OpenSSH Client
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
