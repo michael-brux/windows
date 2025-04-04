27769e3e33e8f9d36187a9f4de9cc64e7644f739
Michael Brux <7270100+michael-brux@users.noreply.github.com>
2025-04-04 20:50:43 +0200

    Update install_openssh.ps1

--- install_openssh.ps1
+++ install_openssh.ps1
@@ -1,1 +1,14 @@
+# Script to install OpenSSH client and server on Windows
 
+# Install OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start and set OpenSSH Server to start automatically
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the installation
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
