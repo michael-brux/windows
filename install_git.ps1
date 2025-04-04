77f755f8e1159fb195ab62fb47d919b805c04f95
Michael Brux <7270100+michael-brux@users.noreply.github.com>
2025-04-04 21:07:52 +0200

    Update install_git.ps1

--- install_git.ps1
+++ install_git.ps1
@@ -1,19 +1,4 @@
 # PowerShell script to install Git on Windows 11 Pro
 
-# Define the Git installer URL
-$gitInstallerUrl = "https://github.com/git-for-windows/git/releases/latest/download/Git-2.40.1-64-bit.exe"
-
-# Define the path where the Git installer will be downloaded
-$gitInstallerPath = "$env:TEMP\Git-Installer.exe"
-
-# Download the Git installer
-Invoke-WebRequest -Uri $gitInstallerUrl -OutFile $gitInstallerPath
-
-# Run the Git installer silently
-Start-Process -FilePath $gitInstallerPath -ArgumentList "/SILENT" -Wait
-
-# Remove the Git installer after installation
-Remove-Item -Path $gitInstallerPath -Force
-
-# Verify the installation
-git --version
+# use winget
+winget install --id Git.Git -e --source winget
